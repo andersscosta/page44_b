27e27fc3f2b078ecddf7dcacf45e5a6ae43cc777
Anderson Costa <andersscosta2@gmail.com>
2021-11-17 10:22:55 -0300

    fist commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let conta = Number(prompt('Qual o valor da conta?'))
+let pessoas = Number(prompt('E qual o total de pessoas?'))
+let valor = conta/pessoas
+alert('O valor que cada um tem que pagar é R$: ' + valor.toFixed(2))
